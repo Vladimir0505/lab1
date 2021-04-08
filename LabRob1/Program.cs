755e04ffe7a894b86943d5e9d74131af2d1856f5
vlpdimirz@gmail.com <vlpdimirz@gmail.com>
2021-04-08 14:02:43 +0300

    Вывод массива

--- LabRob1/Program.cs
+++ LabRob1/Program.cs
@@ -12,13 +12,13 @@ namespace LabRob1
             Console.WriteLine($"{student1} \t {student2}");
             for (int i = 0; i < 10; i++)
             {
-                //student1.SumReiting(); student2.SumReiting(); student1.SrReiting(); student2.SrReiting();
-                Console.WriteLine($"Student №1 {student1[i]}" +
-                    $"  \t Student №2 {student2[i]}");
+                student1.SumReiting(); student2.SumReiting(); student1.SrReiting(); student2.SrReiting();
+                Console.WriteLine($"Student №1 {student1[i]} = {student1.ReitingMasss()}  \t Student №2 {student2[i]} = {student2.ReitingMasss()}");
+               
             }
-            Console.WriteLine($"Балы первого и второго студента { student1.SumReiting()}\t{ student2.SumReiting()} ");
-            Console.WriteLine($"Средний балл первого и втогоро  {student1.SrReiting()}\t{student2.SrReiting()}");
+            Console.WriteLine($"{ student1.SumReiting()} {student1.SrReiting()} {student2.SumReiting()} {student2.SrReiting()} ");
             
+
 
 
         }
